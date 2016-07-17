c629de1aa7b4a15bfb65caf12b057806c666d5e0
jfrey <nomhad@gmail.com>
2016-07-17 21:40:42 +0200

    much simpler and uglier singleton

--- streamer.cpp
+++ streamer.cpp
@@ -4,31 +4,19 @@
 #include <iostream>
 #include <cstdlib>
 
-Streamer::Streamer() {
-    std::cout << "Init streamer" << std::endl;
 
-    // make a new stream_info
-    lsl::stream_info info("MyEventStream","Markers",1,lsl::IRREGULAR_RATE,lsl::cf_string,"myuniquesourceid23443");
+// kinda ugly way to use a kinda singleton. I will not brag about this code.
+lsl::stream_info info("MyEventStream","Markers",1,lsl::IRREGULAR_RATE,lsl::cf_string,"myuniquesourceid23443");
 
-    // make a new outlet
-    outlet = new lsl::stream_outlet(info);
-}
-
-// pushing a string to LSL
-void Streamer::push() {
-  std::string mrk = "test";
-  // and choose the marker to send
-  std::cout << "now sending: " << mrk << std::endl;
-  outlet->push_sample(&mrk);
-}
+lsl::stream_outlet *outlet;
 
 void Streamer::send()
 {
-    // the first time we init our one copy of the object
-  if (singleton == NULL) {
-      singleton = new Streamer();
-    }
-    
+
+  if (outlet == NULL) {
+      outlet = new lsl::stream_outlet(info);
+  }
+
     // send random marker strings
     std::cout << "Now sending markers... " << std::endl;
     while(true) {
@@ -36,7 +24,9 @@ void Streamer::send()
         double endtime = ((double)clock())/CLOCKS_PER_SEC + (rand()%1000)/1000.0;
         while (((double)clock())/CLOCKS_PER_SEC < endtime);
 
-        // now send it (note the &)
-	singleton->push();
+  std::string mrk = "test";
+  // and choose the marker to send
+  std::cout << "now sending: " << mrk << std::endl;
+  outlet->push_sample(&mrk);
     }
 }
