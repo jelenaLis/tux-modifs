03a3571e31c1b321b91fe65fb663320eb57e5989
jfrey <nomhad@gmail.com>
2016-07-17 20:15:26 +0200

    working exmanple for sending LSL stims

--- main.cpp
+++ main.cpp
@@ -170,12 +170,31 @@ int main( int argc, char *argv[] )
 	     "ETRacer comes with ABSOLUTELY NO WARRANTY. "
 	     "This is free software,\nand you are welcome to redistribute "
 	     "it under certain conditions.\n"
-	     "See http://www.gnu.org/copyleft/gpl.html for details.\n\n" );
+	     "See http://www.gnu.org/copyleft/gpl.html for details.\n\n"
+	     "BCI experiment version!\n\n" 
+	     );
 
     // testing LSL
     
     // make a new stream_info
     lsl::stream_info info("MyEventStream","Markers",1,lsl::IRREGULAR_RATE,lsl::cf_string,"myuniquesourceid23443");
+
+    // make a new outlet
+    lsl::stream_outlet outlet(info);
+
+    // send random marker strings
+    std::cout << "Now sending markers... " << std::endl;
+    while(true) {
+        // wait for a random period of time
+        double endtime = ((double)clock())/CLOCKS_PER_SEC + (rand()%1000)/1000.0;
+        while (((double)clock())/CLOCKS_PER_SEC < endtime);
+        // and choose the marker to send
+	std::string mrk = "test";
+	std::cout << "now sending: " << mrk << std::endl;
+
+        // now send it (note the &)
+        outlet.push_sample(&mrk);
+    }
 
     // end testing
     
